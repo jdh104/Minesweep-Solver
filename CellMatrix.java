404524e8d43c47fa845c4487a4ed2857cb68cf49
Jonah <jdh104@latech.edu>
2018-10-20 17:45:36 -0500

    swap j & i values in output statement

--- CellMatrix.java
+++ CellMatrix.java
@@ -66,7 +66,7 @@ public class CellMatrix {
             for (int j=0; j<this.width; j++) {
                 try {
                     if (!matrix.get(i).get(j).isResolved()) {
-                        MainClass.outputToFrame("Validating Cell at: x=" + i + ", y=" + j);
+                        MainClass.outputToFrame("Validating Cell at: x=" + j + ", y=" + i);
                         this.autoValidateCellAt(i, j);
                         matrix.get(i).get(j).setResolvedFlag(true);
                     }
